567f92f5f0ccc7ebe4c431e031f75ff3e82694a2
Iminzah <sharroniminzah@gmail.com>
2021-02-28 17:06:44 +0300

    assignment

---
+++ src/ASSIGNMENT.kt
@@ -0,0 +1,63 @@
+fun main(){name()
+           modulus()
+    var sum =sum(1, 2,3,4)
+    println(sum)
+           aboutme()
+}
+fun name(){
+    var name = "hello Ada"
+}
+fun modulus(){
+    var a = 30
+    var b = git
+    var results =(a%b)
+    println(results)
+}
+fun sum(num1:Int,num2:Int,num3:Int,num4:Int ):Int{
+    var sum=num1 + num2 + num3 + num4
+    return sum
+}
+fun aboutme(){
+    var aboutme=("i love my self")
+            println(aboutme)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
